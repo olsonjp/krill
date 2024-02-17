1d008182eecd21248aeff4a9a4e743306bae71f1
Jacob Olson <olsonjp@users.noreply.github.com>
2024-02-17 10:46:09 -0700

    Initial Django and Data Model Implementation (#2)
    * Clean start for django implementation
    
    Django initial commit
    
    Add gitignore
    
    Deleting pycache files
    
    Initial django configs
    
    * Initial configuration of data model
    
    This gets the bare bones form of the data model in place

--- go.mod
+++
@@ -1,5 +0,0 @@
-module krill
-
-go 1.21.0
-
-require github.com/google/uuid v1.3.1
---
+++ krill/krill/views/home.py
@@ -0,0 +1,49 @@
+import re
+from collections import namedtuple
+
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
+from django.core.cache import cache
+from django.shortcuts import redirect, render
+from django.utils.translation import gettext_lazy as _
+from django.views.generic import View
+
+__all__ = (
+    'HomeView',
+)
+
+class HomeView(View):
+    template_name = 'home.html'
+
+    def get(self, request):
+        """
+        if settings.LOGIN_REQUIRED and not request.user.is_authenticated:
+            return redirect('login')
+
+        # Construct the user's custom dashboard layout
+        try:
+            dashboard = get_dashboard(request.user).get_layout()
+        except Exception:
+            messages.error(request, _(
+                "There was an error loading the dashboard configuration. A default dashboard is in use."
+            ))
+            dashboard = get_default_dashboard(config=DEFAULT_DASHBOARD).get_layout()
+
+        # Check whether a new release is available. (Only for staff/superusers.)
+        new_release = None
+        if request.user.is_staff or request.user.is_superuser:
+            latest_release = cache.get('latest_release')
+            if latest_release:
+                release_version, release_url = latest_release
+                if release_version > version.parse(settings.VERSION):
+                    new_release = {
+                        'version': str(release_version),
+                        'url': release_url,
+                    } """
+
+        return render(request, self.template_name)
+        """, {
+        'dashboard': dashboard,
+        'new_release': new_release,
+        })"""
