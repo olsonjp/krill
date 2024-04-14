c1d12eef638be9ce954192998a947cf38b84e6a6
Jacob Olson <jacob.p.olson87@gmail.com>
2024-04-14 12:47:36 -0600

    Add location to admin panel

--- krill/storage/admin.py
+++ krill/storage/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Site, Device, Shelf, Rack, Box
+from .models import Site, Location, Device, Shelf, Rack, Box
 
 admin.site.register(Site)
+admin.site.register(Location)
 admin.site.register(Device)
 admin.site.register(Shelf)
 admin.site.register(Rack)
