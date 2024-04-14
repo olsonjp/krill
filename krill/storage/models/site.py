fe3de9cfe99a718b0a3776b4a28751cefb8bb255
Jacob Olson <jacob.p.olson87@gmail.com>
2024-04-14 11:20:08 -0600

    Adjusting models for storage and site
    Adjusting the models for storage and site to match models as discussed
    with Matt

--- krill/storage/models/site.py
+++ krill/storage/models/site.py
@@ -8,3 +8,14 @@ class Site(models.Model):
 
     def __str__(self):
         return self.name
+
+class Location(models.Model):
+    """
+    A Location represents a specific building or office where Samples are stored.
+    """
+    name = models.CharField(max_length=200)
+    site = models.ForeignKey(to='storage.Site',
+        on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
