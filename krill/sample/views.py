3cfd995ab17c1a7babb53e83a527e1ca4f292a3a
Jacob Olson <jacob.p.olson87@gmail.com>
2024-05-06 20:05:41 -0600

    Saving progress

--- krill/sample/views.py
+++ krill/sample/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import re
+from collections import namedtuple
+
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
+from django.core.cache import cache
+from django.shortcuts import redirect, render
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import View
+from sample.models import Sample
 
-def index(request):
-    return HttpResponse('Hello, welcome to the index page.')
+__all__ = (
+    'SampleView',
+)
 
-class TempSampleView(View):
+class SampleView(View):
     template_name = 'sample/sample.html'
 
+    samples = Sample.objects.all()
+    print(samples)
+
+    context = {
+        'samples':samples
+    }
+
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, self.context)
