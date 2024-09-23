55048b3ccbb9980ed0ede90f28dff2ec33798341
Jacob Olson <olsonjp@users.noreply.github.com>
2024-09-22 22:10:50 -0600

    Remove python from golang in progress (#8)

--- krill/sample/models/sample.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from storage.models import Box
-
-class Sample(models.Model):
-    name = models.CharField(max_length=200)
-    notes = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
--- krill/storage/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from .storage import *
-from .site import *
