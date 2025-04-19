d681bde5901490b994df4b20d30509685fb059f2
Jacob Olson <olsonjp@users.noreply.github.com>
2025-04-19 16:43:02 -0600

    Adding buttons (#16)

--- krill/sample/models/aliquot.py
+++ krill/sample/models/aliquot.py
@@ -3,7 +3,7 @@ from storage.models import Box
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 class AliquotType(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, unique=True, null=False, blank=False)
     description = models.CharField(max_length=200, blank=True)
     def __str__(self):
         return self.name
@@ -14,8 +14,8 @@ class AliquotDisposition(models.Model):
         ('exhausted', 'Exhausted'),
         ('in_use', 'In Use'),
     )
-    name = models.CharField(max_length=200)
-    dispositionType = models.CharField(max_length = 32, choices = TYPES, default = 'stored')
+    name = models.CharField(max_length=200, unique=True, null=False, blank=False)
+    dispositionType = models.CharField(max_length = 32, choices = TYPES, default = 'stored', null=False, blank=False)
     description = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
@@ -24,17 +24,17 @@ class AliquotDisposition(models.Model):
 
 class Aliquot(models.Model):
     parent = models.ForeignKey(to='Aliquot', on_delete=models.PROTECT, null=True, blank=True)
-    sample = models.ForeignKey(to='sample.Sample', on_delete=models.PROTECT)
-    quantity = models.IntegerField(default=0)
-    aliquotType = models.ForeignKey(to='AliquotType', on_delete=models.PROTECT)
-    disposition = models.ForeignKey(to='AliquotDisposition', on_delete=models.PROTECT)
+    sample = models.ForeignKey(to='sample.Sample', on_delete=models.PROTECT, null=False, blank=False)
+    quantity = models.IntegerField(default=0, null=False, blank=False)
+    aliquotType = models.ForeignKey(to='AliquotType', on_delete=models.PROTECT, null=False, blank=False)
+    disposition = models.ForeignKey(to='AliquotDisposition', on_delete=models.PROTECT, null=False, blank=False)
     passage = models.CharField(max_length=200, default=0)
     experiment = models.TextField(blank=True)
     notes = models.TextField(blank=True)
 
 class AliquotLocation(models.Model):
-    aliquot = models.ForeignKey(to='Aliquot', on_delete=models.CASCADE)
-    box = models.ForeignKey(to='storage.Box', on_delete=models.PROTECT)
-    row = models.PositiveSmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10)])
-    column = models.PositiveSmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10)])
+    aliquot = models.ForeignKey(to='Aliquot', on_delete=models.PROTECT, null=False, blank=False)
+    box = models.ForeignKey(to='storage.Box', on_delete=models.PROTECT, null=False, blank=False)
+    row = models.PositiveSmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10)], null=False, blank=False)
+    column = models.PositiveSmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(10)], null=False, blank=False)
     
---
+++ krill/storage/views/create.py
@@ -0,0 +1,32 @@
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from ..models.storage import Box, Rack, Shelf, Device
+from ..models.site import Site
+from ..forms import BoxForm, ShelfForm, DeviceForm, SiteForm
+
+class StorageCreateView(CreateView):
+    template_name = 'storage/create.html'
+    
+    def get_form_class(self):
+        model_type = self.request.GET.get('type', 'site')
+        
+        if model_type == 'box':
+            return BoxForm
+        elif model_type == 'shelf':
+            return ShelfForm
+        elif model_type == 'device':
+            return DeviceForm
+        else:
+            return SiteForm
+    
+    def get_success_url(self):
+        model_type = self.request.GET.get('type', 'site')
+        return f"{reverse_lazy('storage:list')}?type={model_type}"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        model_type = self.request.GET.get('type', 'site')
+        context['model_type'] = model_type
+        context['model_name'] = model_type.title()
+        return context 
