6fa76bdcab9d617225363a06511d095339db4017
Jacob Olson <olsonjp@users.noreply.github.com>
2025-08-30 17:46:46 -0600

    Fix the capacity endpoint and add TODOs (#19)

--- krill/storage/urls.py
+++ krill/storage/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views.list import StorageListView
 from .views.detail import StorageDetailView
 from .views.create import StorageCreateView
+from .views.capacity import box_capacity
 
 app_name = 'storage'
 
@@ -9,5 +10,6 @@ urlpatterns = [
     path('list/', StorageListView.as_view(), name='list'),
     path('detail/<str:type>/<int:pk>/', StorageDetailView.as_view(), name='detail'),
     path('create/', StorageCreateView.as_view(), name='create'),
+    path('capacity/', box_capacity, name='capacity'),
     # ... other existing urls ...
 ]
