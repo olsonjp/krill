17d7fadabff2b8ed52c71c3ca6954919e8203078
Jacob Olson <olsonjp@users.noreply.github.com>
2025-08-31 17:14:13 -0600

    Adding auto-storage of tubes (#29)
    A bunch of improvements to and linking of aliquots and storage locations

--- krill/sample/urls.py
+++ krill/sample/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views.list import SampleListView
-from .views.detail import ModelDetailView
+from .views.detail import ModelDetailView, TubeDetailView
 from .views.create import ModelCreateView
 
 app_name = 'sample'
@@ -8,6 +8,7 @@ app_name = 'sample'
 urlpatterns = [
     path('list/', SampleListView.as_view(), name='list'),
     path('detail/<str:type>/<int:pk>/', ModelDetailView.as_view(), name='detail'),
+    path('tube/<int:pk>/', TubeDetailView.as_view(), name='tube_detail'),
     path('create/', ModelCreateView.as_view(), name='create'),
     # ... other existing urls ...
 ]
