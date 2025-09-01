62b1e5e91b0fcba68e5499fff32e01274563178f
Jacob Olson <olsonjp@users.noreply.github.com>
2025-09-01 10:39:59 -0600

    Adding pagination to list views (#40)

--- krill/sample/views/list.py
+++ krill/sample/views/list.py
@@ -1,36 +1,88 @@
 from django.views.generic import ListView
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
 from ..models.sample import Sample
 from ..models.aliquot import Aliquot, AliquotType, AliquotLocation
 from ..models.source import Source
 
-@method_decorator(login_required, name='dispatch')
-class SampleListView(ListView):
+class SampleListView(LoginRequiredMixin, ListView):
     template_name = 'sample/list.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    paginate_by = 20
+    context_object_name = 'items'
+    
+    def get_queryset(self):
         model_type = self.request.GET.get('type', 'sample')
+        
+        # Get the queryset based on model type
         if model_type == 'aliquot':
-            context['items'] = Aliquot.objects.select_related(
+            queryset = Aliquot.objects.select_related(
                 'sample', 
                 'aliquot_type'
             ).prefetch_related(
                 'locations__box',
-                'tubes__disposition'
-            ).all()
+                'tubes__disposition',
+                'tubes'
+            )
+        elif model_type == 'aliquot-type':
+            queryset = AliquotType.objects.all()
+        elif model_type == 'source':
+            queryset = Source.objects.all()
+        else:  # default to samples
+            queryset = Sample.objects.select_related('source')
+        
+        # Apply search filtering if provided
+        search_query = self.request.GET.get('q', '')
+        if search_query:
+            if model_type == 'aliquot':
+                queryset = queryset.filter(
+                    name__icontains=search_query
+                ) | queryset.filter(
+                    sample__name__icontains=search_query
+                )
+            elif model_type == 'sample':
+                queryset = queryset.filter(name__icontains=search_query)
+            elif model_type == 'source':
+                queryset = queryset.filter(name__icontains=search_query)
+            elif model_type == 'aliquot-type':
+                queryset = queryset.filter(name__icontains=search_query)
+        
+        # Apply sorting
+        sort_by = self.request.GET.get('sort', 'name')
+        sort_order = self.request.GET.get('order', 'asc')
+        
+        if sort_order == 'desc':
+            sort_by = f'-{sort_by}'
+        
+        # Apply sorting to queryset
+        if hasattr(queryset.model, sort_by.replace('-', '')):
+            queryset = queryset.order_by(sort_by)
+        else:
+            # Default sorting - use appropriate field for consistent pagination
+            if model_type == 'aliquot':
+                queryset = queryset.order_by('sample__name', 'id')
+            elif hasattr(queryset.model, 'name'):
+                queryset = queryset.order_by('name', 'id')
+            else:
+                queryset = queryset.order_by('id')
+        
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        model_type = self.request.GET.get('type', 'sample')
+        
+        # Set model name
+        if model_type == 'aliquot':
             context['model_name'] = 'Aliquots'
         elif model_type == 'aliquot-type':
-            context['items'] = AliquotType.objects.all()
             context['model_name'] = 'Aliquot Types'
         elif model_type == 'source':
-            context['items'] = Source.objects.all()
             context['model_name'] = 'Sources'
         else:  # default to samples
-            context['items'] = Sample.objects.all()
             context['model_name'] = 'Samples'
-        return context
-
-    def get_queryset(self):
-        return []  # We're using context['items'] instead 
+        
+        # Add search and sort context
+        context['search_query'] = self.request.GET.get('q', '')
+        context['sort_by'] = self.request.GET.get('sort', 'name')
+        context['sort_order'] = self.request.GET.get('order', 'asc')
+        
+        return context 
