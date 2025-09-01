46528094455275872489a08c2c09c9f4f70c5ab1
Jacob Olson <jacob.p.olson87@gmail.com>
2025-09-01 10:39:29 -0600

    Adding pagination to list views

--- krill/storage/views/list.py
+++ krill/storage/views/list.py
@@ -1,33 +1,90 @@
 from django.views.generic import ListView
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
 from ..models.storage import Device, Shelf, Rack, Box
 from ..models.site import Site
 
-@method_decorator(login_required, name='dispatch')
-class StorageListView(ListView):
+class StorageListView(LoginRequiredMixin, ListView):
     template_name = 'storage/list.html'
+    paginate_by = 20
+    context_object_name = 'items'
+    
+    def get_queryset(self):
+        model_type = self.request.GET.get('type', 'site')
+        
+        # Get the queryset based on model type
+        if model_type == 'box':
+            queryset = Box.objects.select_related(
+                'rack__shelf__device__site'
+            )
+        elif model_type == 'shelf':
+            queryset = Shelf.objects.select_related(
+                'device__site'
+            )
+        elif model_type == 'device':
+            queryset = Device.objects.select_related('site')
+        else:  # default to sites
+            queryset = Site.objects.all()
+        
+        # Apply search filtering if provided
+        search_query = self.request.GET.get('q', '')
+        if search_query:
+            if model_type == 'box':
+                queryset = queryset.filter(
+                    name__icontains=search_query
+                ) | queryset.filter(
+                    rack__shelf__device__name__icontains=search_query
+                )
+            elif model_type == 'shelf':
+                queryset = queryset.filter(
+                    name__icontains=search_query
+                ) | queryset.filter(
+                    device__name__icontains=search_query
+                )
+            elif model_type == 'device':
+                queryset = queryset.filter(
+                    name__icontains=search_query
+                ) | queryset.filter(
+                    site__name__icontains=search_query
+                )
+            elif model_type == 'site':
+                queryset = queryset.filter(name__icontains=search_query)
+        
+        # Apply sorting
+        sort_by = self.request.GET.get('sort', 'name')
+        sort_order = self.request.GET.get('order', 'asc')
+        
+        if sort_order == 'desc':
+            sort_by = f'-{sort_by}'
+        
+        # Apply sorting to queryset
+        if hasattr(queryset.model, sort_by.replace('-', '')):
+            queryset = queryset.order_by(sort_by)
+        else:
+            # Default sorting - use id as fallback for consistent pagination
+            if hasattr(queryset.model, 'name'):
+                queryset = queryset.order_by('name', 'id')
+            else:
+                queryset = queryset.order_by('id')
+        
+        return queryset
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         model_type = self.request.GET.get('type', 'site')
+        
+        # Set model name
         if model_type == 'box':
-            context['items'] = Box.objects.select_related(
-                'rack__shelf__device__site'
-            ).all()
             context['model_name'] = 'Boxes'
         elif model_type == 'shelf':
-            context['items'] = Shelf.objects.select_related(
-                'device__site'
-            ).all()
             context['model_name'] = 'Shelves'
         elif model_type == 'device':
-            context['items'] = Device.objects.select_related('site').all()
             context['model_name'] = 'Devices'
         else:  # default to sites
-            context['items'] = Site.objects.all()
             context['model_name'] = 'Sites'
-        return context
-
-    def get_queryset(self):
-        return []  # We're using context['items'] instead 
+        
+        # Add search and sort context
+        context['search_query'] = self.request.GET.get('q', '')
+        context['sort_by'] = self.request.GET.get('sort', 'name')
+        context['sort_order'] = self.request.GET.get('order', 'asc')
+        
+        return context 
