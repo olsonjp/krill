4743ae558389a32c7685a61677c4d8d37ff849cb
Jacob Olson <jacob.p.olson87@gmail.com>
2026-05-25 18:56:39 -0600

    fix: validate filter params and fix disposition sort in SampleListView
    - Validate ?disposition= against DISPOSITION_CHOICES to prevent silent empty results
    - Guard ?aliquot_type= against non-integer values to prevent 500 crash
    - Disposition sort now sorts by disposition_type (was silently falling back to sample name)
    - Scope filter context keys to aliquot view to avoid extra queries on other list types
    
    Part of #118

--- krill/sample/views/list.py
+++ krill/sample/views/list.py
@@ -40,12 +40,16 @@ class SampleListView(LoginRequiredMixin, ListView):
         # Aliquot-specific filters
         if model_type == 'aliquot':
             disposition_filter = self.request.GET.get('disposition', '')
-            if disposition_filter:
+            valid_dispositions = [c[0] for c in AliquotDisposition.DISPOSITION_CHOICES]
+            if disposition_filter and disposition_filter in valid_dispositions:
                 queryset = queryset.filter(disposition__disposition_type=disposition_filter)
 
             aliquot_type_filter = self.request.GET.get('aliquot_type', '')
             if aliquot_type_filter:
-                queryset = queryset.filter(aliquot_type__id=aliquot_type_filter)
+                try:
+                    queryset = queryset.filter(aliquot_type__id=int(aliquot_type_filter))
+                except (ValueError, TypeError):
+                    pass
 
         # Sorting
         sort_by = self.request.GET.get('sort', 'name')
@@ -57,7 +61,7 @@ class SampleListView(LoginRequiredMixin, ListView):
             sort_prefix = ''
 
         if model_type == 'aliquot' and sort_by == 'disposition':
-            queryset = queryset.order_by('sample__name', 'id')
+            queryset = queryset.order_by(f'{sort_prefix}disposition__disposition_type', 'id')
         elif model_type == 'aliquot' and sort_by == 'type':
             queryset = queryset.order_by(f'{sort_prefix}aliquot_type__name', 'id')
         elif model_type == 'aliquot' and sort_by == 'sample':
@@ -104,9 +108,15 @@ class SampleListView(LoginRequiredMixin, ListView):
         context['sort_order'] = self.request.GET.get('order', 'asc')
 
         # Aliquot filter context
-        context['disposition_filter'] = self.request.GET.get('disposition', '')
-        context['aliquot_type_filter'] = self.request.GET.get('aliquot_type', '')
-        context['disposition_choices'] = AliquotDisposition.DISPOSITION_CHOICES
-        context['aliquot_types'] = AliquotType.objects.order_by('name')
+        if model_type == 'aliquot':
+            context['disposition_filter'] = self.request.GET.get('disposition', '')
+            context['aliquot_type_filter'] = self.request.GET.get('aliquot_type', '')
+            context['disposition_choices'] = AliquotDisposition.DISPOSITION_CHOICES
+            context['aliquot_types'] = AliquotType.objects.order_by('name')
+        else:
+            context['disposition_filter'] = ''
+            context['aliquot_type_filter'] = ''
+            context['disposition_choices'] = []
+            context['aliquot_types'] = AliquotType.objects.none()
 
         return context
